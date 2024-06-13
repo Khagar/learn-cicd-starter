4a07dd7c48d881962eacc57948b766181ef10f25
Khagar <karol.jasina94@gmail.com>
2024-06-13 22:17:00 +0200

    fix sec

--- json.go
+++ json.go
@@ -26,6 +26,7 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		w.WriteHeader(500)
 		return
 	}
-	w.WriteHeader(code)
-	w.Write(dat)
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
 }
